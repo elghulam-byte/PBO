301c76109911b3a2569c4cfb63ceccf626c4735d
elghulam-byte <ghulamelfirdausy290307@gmail.com>
2026-09-08 18:28:58 +0700

    Percobaan4_1

---
+++ P2/percobaan4/BagiKelompok.java
@@ -0,0 +1,17 @@
+package percobaan4;
+
+public class BagiKelompok {
+
+    Run | Debug
+
+    public static void main(String[] args) {
+        System.out.println("awal program");
+        int jumlahMahasiswa = 32;
+        int jumlahKelompok = 0;
+        int anggota PerKelompok = jumlahMahasiswa / jumlahKelompok;
+        System.out.println(anggota PerKelompok
+        );
+System.out.println("akhir program");
+    }
+}
+
