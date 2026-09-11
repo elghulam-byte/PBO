df746800dd62ebeacf32018b0cfe117d4bf7a7cb
elghulam-byte <ghulamelfirdausy290307@gmail.com>
2026-09-11 09:11:43 +0700

    P3_Percobaan2

---
+++ Motor.java
@@ -0,0 +1,13 @@
+public class Motor {
+    public int kecepatan = 0;
+    public boolean kontakOn = false;
+
+    public void printStatus() {
+        if (kontakOn == true) {
+            System.out.println("kontak on");
+        } else {
+            System.out.println("kontak off");
+        }
+        System.out.println("kecepatan " + kecepatan + "\n");
+    }
+}
---
+++ motorDemo.java
@@ -0,0 +1,8 @@
+public class motorDemo {
+    public static void main(String[] args) {
+        Motor motor = new Motor();
+        motor.printStatus();
+        motor.kecepatan = 50;
+        motor.printStatus();
+    }
+}
