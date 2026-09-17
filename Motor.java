56e85667078f81e55a06b58d9bf370b8aa82d6f2
elghulam-byte <ghulamelfirdausy290307@gmail.com>
2026-09-17 22:28:53 +0700

    P3_Percobaan2_Pertanyaan1

--- Motor.java
+++ Motor.java
@@ -1,13 +1,38 @@
 public class Motor {
-    public int kecepatan = 0;
-    public boolean kontakOn = false;
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
 
-    public void printStatus() {
+    public void nyalakanMesin() {
+        kontakOn = true;
+    } 
+
+    public void matikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
         if (kontakOn == true) {
-            System.out.println("kontak on");
+            kecepatan += 5;
         } else {
-            System.out.println("kontak off");
+            System.out.println("Kecepatan tidak bisa bertambah karena mesin off\n");
         }
-        System.out.println("kecepatan " + kecepatan + "\n");
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena mesin off\n");
+        }
+    }
+
+    public void printStatus () {
+        if (kontakOn == true) {
+            System.out.println("Kontak On");
+        } else {
+            System.out.println("Kontak Off");
+        }
+        System.out.println("Kecepatan " + kecepatan + "\n");
     }
 }
--- motorDemo.java
+++ motorDemo.java
@@ -1,8 +1,18 @@
-public class motorDemo {
+public class MotorDemo {
+
     public static void main(String[] args) {
         Motor motor = new Motor();
         motor.printStatus();
-        motor.kecepatan = 50;
+        motor.tambahKecepatan();
+        motor.nyalakanMesin();
+        motor.printStatus();
+        motor.tambahKecepatan();
+        motor.printStatus();
+        motor.tambahKecepatan();
+        motor.printStatus();
+        motor.tambahKecepatan();
+        motor.printStatus();
+        motor.matikanMesin();
         motor.printStatus();
     }
 }
