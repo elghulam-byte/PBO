0989bff2e04da9160ad89054622c76306d937a3a
elghulam-byte <ghulamelfirdausy290307@gmail.com>
2026-09-24 23:00:44 +0700

    P4_Percobaan5

---
+++ P4/percobaan5/MainPercobaan5.java
@@ -0,0 +1,9 @@
+package P4.percobaan5;
+
+public class MainPercobaan5 {
+
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil("Avanza");
+        mobil.tampilkanInfo();
+    }
+}
